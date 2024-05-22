32bc8d452e9c4219c4c0fd9837f1e66acac93f8b
Peter Yu <peter890331@gmail.com>
2024-05-23 00:23:12 +0800

    Time: 472 ms (75.15%), Space: 25.7 MB (91.72%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        self.s = s
+        self.n = len(s)
+        self.dp = [[0] * self.n for _ in range(self.n)]
+        self.ans = []
+        
+        for i in range(self.n):
+            self.dp[i][i] = 1
+        
+        for i in range(self.n-1):
+            self.dp[i][i+1] = int(s[i]==s[i+1])
+        
+        for length  in range(3, self.n+1):
+            for i in range(self.n-length +1):
+                j = i+length -1
+                if s[i]==s[j]:
+                    self.dp[i][j] = self.dp[i+1][j-1]
+                else:
+                    self.dp[i][j] = 0
+        
+        ans_ = []
+        self.dfs(0, ans_)
+        
+        return self.ans
+    
+    def dfs(self, i, ans_):
+        if i==self.n:
+            self.ans.append(ans_[:])
+            return
+        
+        for j in range(i, self.n):
+            if self.dp[i][j]:
+                ans_.append(self.s[i:j+1])
+                self.dfs(j+1, ans_)
+                ans_.pop()
