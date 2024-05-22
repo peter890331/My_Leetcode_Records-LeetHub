37fd4ac9151af16cf4b8026de927f6f3f10b4ce6
Peter Yu <peter890331@gmail.com>
2024-05-23 00:41:31 +0800

    Time: 109 ms (34.13%), Space: 79.2 MB (37.15%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -13,21 +13,19 @@ public:
             dp[i][i] = 1;
 
         for (int i=0; i+1<n; i++)
-            dp[i][i+1] = s[i]==s[i+1];
+            if (s[i]==s[i+1])
+                dp[i][i+1] = 1;
 
         for (int len=3; len<=n; len++)
             for (int i=0; i+len-1<n; i++)
             {
                 int j = i+len-1;
-                if (s[i]==s[j])
-                    dp[i][j] = dp[i+1][j-1];
-                else
-                    dp[i][j] = 0;
+                if (s[i]==s[j] && dp[i+1][j-1])
+                    dp[i][j] = 1;
             }
 
         vector<string> ans_;
         dfs(0, ans_);
-
         return ans;
     }
 
@@ -38,6 +36,7 @@ public:
             ans.push_back(ans_);
             return;
         } 
+        
         for (int j=i; j<n; j++)
         {
             if (dp[i][j])
