b3579810ce6bc90999c7169724d3dc39341d02ff
Peter Yu <peter890331@gmail.com>
2024-05-31 21:28:45 +0800

    Time: 0 ms (100%), Space: 14.3 MB (18.19%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int n : nums) {
+            freq[n]++;
+        }
+
+        vector<int> res;
+        for (auto& pair : freq) {
+            if (pair.second == 1) {
+                res.push_back(pair.first);
+            }
+        }
+
+        return res;
+    }
+};
+
+// heir-of-god's solution.
