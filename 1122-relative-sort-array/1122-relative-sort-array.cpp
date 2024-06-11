0967cb9014bce3a22588698b9852f59171ddfddd
Peter Yu <peter890331@gmail.com>
2024-06-11 16:01:52 +0800

    Time: 5 ms (34.15%), Space: 10.1 MB (41.93%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        unordered_map<int, int> count;
+        vector<int> ans;
+        vector<int> remaining;
+        int n1 = arr1.size();
+        int n2 = arr2.size();
+
+        for (int i=0; i<n1; i++) {
+            count[arr1[i]]++;
+        }
+
+        for (int i=0; i<n2; i++) {
+            int temp = arr2[i];
+            while (count[temp] > 0) {
+                ans.push_back(temp);
+                count[temp]--;
+            }
+            count.erase(temp);
+        }
+
+        for (auto it: count) {
+            while (it.second > 0) {
+                remaining.push_back(it.first);
+                it.second--;
+            }
+        }
+
+        sort(remaining.begin(), remaining.end());
+
+        for (int i=0; i<remaining.size(); i++) {
+            ans.push_back(remaining[i]);
+        }
+
+        return ans;
+
+    }
+};
