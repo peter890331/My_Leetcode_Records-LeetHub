5c7fd90a436c52ff43ae5d2c97e3aa9cfeb03d53
Peter Yu <peter890331@gmail.com>
2024-09-06 21:32:30 +0800

    Time: 461 ms (41.7%), Space: 262.7 MB (44.44%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.cpp
@@ -13,11 +13,11 @@ public:
     // 函数功能：删除链表中所有值在 nums 数组中的节点，返回修改后的链表头节点。
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
 
-    // 将 nums 数组中的元素存入 set 中，以便快速查找是否需要删除某个节点，
-    // 使用 set 是因为它具有 O(1) 的查找时间复杂度。这样可以快速判断链表中的节点值是否存在于 nums 中。
-    set<int> st(nums.begin(), nums.end());
+    // 将 nums 数组中的元素存入 unordered_set 中，以便快速查找是否需要删除某个节点，
+    // 使用 unordered_set 是因为它的 count 具有 O(1) 的时间复杂度。这样可以快速判断链表中的节点值是否存在于 nums 中。
+    unordered_set<int> st(nums.begin(), nums.end());
 
-    // 创建一个临时节点 temp，值为 0，并让它的 next 指向链表的头节点
+    // 创建一个临时节点 temp，值为 0，并让它的 next 指向链表的头节点，
     // 这样可以方便地处理链表的第一个节点删除情况。
     ListNode* temp = new ListNode(0);
     temp->next = head;
@@ -27,7 +27,7 @@ public:
 
     // 遍历链表。
     while(temp->next){
-        // 如果当前节点的值存在于 set 中，则需要删除该节点。
+        // 如果当前节点的值存在于 unordered_set 中，则需要删除该节点。
         if(st.count(temp->next->val)){
             // 将要删除的节点保存到临时指针 d 中。
             ListNode* d = temp->next;
@@ -50,4 +50,4 @@ public:
 
 // 在代码中，d 这个临时指针的主要作用是确保删除节点后能够正确释放内存。这是因为在链表操作中，直接断开节点的连接后，需要手动管理内存，防止内存泄漏。
 // 删除节点时需要释放内存： 当 temp->next 指向要删除的节点时，如果你只是修改指针，让 temp->next = temp->next->next;，
-// 这只是让链表跳过了这个节点，节点本身仍然存在于内存中，并没有被释放。如果没有释放它，它将变成“悬空指针”（dangling pointer），导致内存泄漏。
+// 这只是让链表跳过了这个节点，节点本身仍然存在于内存中，并没有被释放。如果没有释放它，它将变成 “悬空指针”（dangling pointer），导致内存泄漏。
