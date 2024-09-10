43dfe10c413d281b866b3445ab708e77333b12fc
Peter Yu <peter890331@gmail.com>
2024-09-10 17:31:31 +0800

    Time: 44 ms (36.52%), Space: 35.6 MB (49.64%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
@@ -26,8 +26,8 @@ public:
     }
 private:
     int GCD(int num1, int num2) {
-        if(num2==0) return num1;
-        return GCD(num2,num1%num2);
+        if(num2 == 0) return num1;
+        return GCD(num2, num1%num2);
     }
 };
 
