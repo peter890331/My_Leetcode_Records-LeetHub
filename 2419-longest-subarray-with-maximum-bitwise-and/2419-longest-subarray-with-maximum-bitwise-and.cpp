4a00925957e464356e7b55c2601560138795c588
Peter Yu <peter890331@gmail.com>
2024-09-15 01:12:40 +0800

    Time: 118 ms (11.18%), Space: 84.9 MB (34.21%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -3,11 +3,19 @@ public:
     int longestSubarray(vector<int>& nums) {
         int maxValue = *max_element(nums.begin(), nums.end());
         int ans = 0;
+        int temp = 0;
+        
         for (int i=0; i<nums.size(); i++){
             if (nums[i] == maxValue){
-                ans++;
+                temp++;
+            }
+            else{
+                ans = max(ans, temp);
+                temp = 0;
             }
         } 
-        return ans;
+        return ans = max(ans, temp);
     }
 };
+
+// Reference from C++数组或vector求最大值最小值, https://www.cnblogs.com/Tang-tangt/p/9352093.html.
