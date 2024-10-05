3d55bc9129baa89f4530ca4b95cae57fe32c0e45
Peter Yu <peter890331@gmail.com>
2024-10-05 20:07:01 +0800

    Time: 15 ms (30.44%), Space: 10.6 MB (30.01%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -16,7 +16,7 @@ public:
             s2_map[s2[i]]++;
         }
 
-        if (map_compare(s1_map, s2_map)) return true;
+        if (s1_map == s2_map) return true;
 
         for (int i = s1_n; i < s2_n; i++) {
             s2_map[s2[i]]++;
@@ -24,17 +24,12 @@ public:
             if (s2_map[s2[i - s1_n]] == 0) {
                 s2_map.erase(s2[i - s1_n]);
             }
-            if (map_compare(s1_map, s2_map)) return true;
+            if (s1_map == s2_map) return true;
         }
 
         return false;
 
     }
-
-    bool map_compare(unordered_map<char, int> &s1_map, unordered_map<char, int> &s2_map){
-        return s1_map == s2_map;
-    }
-
 };
 
 // sliding window.
