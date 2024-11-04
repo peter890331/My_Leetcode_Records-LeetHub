13e2b18dfd36f61aa6567bae7179771cce1646f5
Peter Yu <peter890331@gmail.com>
2024-11-04 20:29:42 +0800

    Time: 47 ms (35.9%), Space: 30 MB (9.87%) - LeetHub

---
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        char temp = word[0];
+        int count = 1;
+        string ans = "";
+
+        for (int i=1; i<word.size(); i++){
+
+            if (temp == word[i]){
+                if (count<9) count++;
+                else{
+                    ans += to_string(count);
+                    ans += temp;
+                    temp = word[i];
+                    count = 1;
+                }
+            }
+
+            else{
+                ans += to_string(count);
+                ans += temp;
+                temp = word[i];
+                count = 1;
+            }
+
+        }
+
+        ans += to_string(count);
+        ans += temp;
+        
+        return ans;
+    }
+};
