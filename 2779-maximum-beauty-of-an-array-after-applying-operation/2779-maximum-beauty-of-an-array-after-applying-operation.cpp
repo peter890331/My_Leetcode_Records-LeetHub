3ea87f39ce90f861cae7c7cd039d455618e784fd
Peter Yu <peter890331@gmail.com>
2024-12-11 17:05:28 +0800

    Time: 36 ms (88.82%), Space: 104.9 MB (84.24%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int maximumBeauty(vector<int>& A, int k) {
+
         sort(A.begin(), A.end());
+
+        // for (int a: A)
+        //     cout<<a<<',';
+        // cout<<endl;
+
         int i = 0, j, n = A.size();
-        for (j = 0; j < n; ++j)
+        for (j = 0; j < n; ++j){
             if (A[j] - A[i] > k * 2)
                 i++;
+            // cout<<i<<','<<j<<endl;
+        }
+
         return j - i;
+
     }
 };
 
