ed852161f5776d4ab4f5b4b8011c05ccacd2f77d
Peter Yu <peter890331@gmail.com>
2024-12-17 02:37:11 +0800

    Time: 0 ms (100%), Space: 27.1 MB (45.93%) - LeetHub

--- 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         int minPosition = 0;
         for (int i=0; i<k; i++){
-            minPosition = min_element(nums.begin(),nums.end()) - nums.begin();
+            minPosition = min_element(nums.begin(), nums.end()) - nums.begin();
             nums[minPosition] = nums[minPosition] * multiplier;
         }
         return nums;
