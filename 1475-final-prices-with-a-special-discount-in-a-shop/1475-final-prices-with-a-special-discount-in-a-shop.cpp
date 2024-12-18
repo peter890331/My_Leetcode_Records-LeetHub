5abbeb9745a5474fc1c0413f55da9e89e815223a
Peter Yu <peter890331@gmail.com>
2024-12-18 16:20:19 +0800

    Time: 0 ms (100%), Space: 14 MB (28.27%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        for (int i=0; i<n; i++){
+            int j = i+1;
+            while (j < n){
+                if (prices[j] <= prices[i]){
+                    prices[i] = prices[i] - prices[j];
+                    break;
+                }
+                j++;
+            }
+        }
+        return prices;
+    }
+};
