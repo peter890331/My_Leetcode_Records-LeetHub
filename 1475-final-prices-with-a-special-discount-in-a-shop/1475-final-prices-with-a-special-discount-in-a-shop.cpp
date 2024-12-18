e94f5cee672b0e8de8bd8b73d579cee6a04d7e0f
Peter Yu <peter890331@gmail.com>
2024-12-18 16:22:03 +0800

    Time: 0 ms (100%), Space: 14.2 MB (9.86%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
         int n = prices.size();
+        int j = 0;
         for (int i=0; i<n; i++){
-            int j = i+1;
+            j = i + 1;
             while (j < n){
                 if (prices[j] <= prices[i]){
                     prices[i] = prices[i] - prices[j];
