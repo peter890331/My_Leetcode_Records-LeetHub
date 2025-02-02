c379f924a3b699873de27a712a2daf5dbfa2efae
Peter Yu <peter890331@gmail.com>
2025-02-02 22:21:29 +0800

    Time: 0 ms (100%), Space: 11.3 MB (22.96%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,16 +4,17 @@ public:
         int count = 0;
         int n = nums.size()-1;
         for (int i=0; i<n; i++){
-            if (nums[i] > nums[i+1]){
+            if (nums[i] > nums[i+1])
                 count++;
-            }
         }
-        if (nums[n] > nums[0]){
+        if (nums[n] > nums[0])
             count++;
-        }
+
         if (count<2)
             return true;
         else 
             return false;
     }
 };
+
+// Reference from Day30__C語言刷LeetCode_Sort, https://ithelp.ithome.com.tw/articles/10350000?sc=rss.qu.
