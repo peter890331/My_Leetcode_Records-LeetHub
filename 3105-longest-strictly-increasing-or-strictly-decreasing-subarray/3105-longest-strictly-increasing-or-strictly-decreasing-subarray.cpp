b7d65cdddb3f56be7af745e8b3bcd81ea42669a0
Peter Yu <peter890331@gmail.com>
2025-02-03 23:22:00 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int max_in = 0;
+        int max_de = 0;
+        int temp_in = 1;
+        int temp_de = 1;
+        int n = nums.size();
+        for (int i=1; i<n; i++){
+            if (nums[i] > nums[i-1]){
+                temp_in++;
+                max_de = max(max_de, temp_de);
+                temp_de = 1;
+            }
+            else if (nums[i] < nums[i-1]){
+                temp_de++;
+                max_in = max(max_in, temp_in);
+                temp_in = 1;
+            }
+            else {
+                temp_in = 1;
+                temp_de = 1;
+            }
+            max_de = max(max_de, temp_de);
+            max_in = max(max_in, temp_in);
+        }
+        return max(max_in, max_de);
+    }
+};
