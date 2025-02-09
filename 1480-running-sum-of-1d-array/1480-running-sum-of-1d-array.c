0ff5aca44b72a64a073aa0e353f8adbc403ad177
Peter Yu <peter890331@gmail.com>
2025-02-10 00:56:44 +0800

    Time: 0 ms (100%), Space: 11.2 MB (79.27%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
@@ -4,7 +4,8 @@
 int* runningSum(int* nums, int numsSize, int* returnSize) {
     int* ans = (int*)malloc(numsSize * sizeof(int));
     *returnSize = numsSize;
-    ans[0] = nums[0];
+    // ans[0] = nums[0];
+    *ans = *nums;
     for (int i=1; i<numsSize; i++){
         ans[i] = nums[i] + ans[i-1];
     }
