aa4c39bfb4a18a174804b49e81a4dbf71b56e638
Peter Yu <peter890331@gmail.com>
2025-02-14 01:02:04 +0800

    Time: 0 ms (100%), Space: 8.6 MB (20.32%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while (x != 0){
+            if ((ans > INT_MAX / 10) || (ans < INT_MIN / 10)){  // INT_MAX / 10 和 INT_MIN / 10 分別是 int 類型能夠表示的最大和最小值除以 10 的結果。
+                return 0;
+            }
+            ans *= 10;
+            ans += x % 10;
+            x /= 10;
+        }
+        return ans;
+    }
+};
