225c24b2b2ff0f4804dcdb053c848147e1a92075
Peter Yu <peter890331@gmail.com>
2025-02-18 16:05:50 +0800

    Time: 0 ms (100%), Space: 7.8 MB (73.43%) - LeetHub

---
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string smallestNumber(string pattern) {
+        pattern = 'I' + pattern;
+        int n = pattern.size();
+
+        int max = 0;
+        vector<int> arr;
+        for (int i=0; i<n; i++){
+            int j = i+1;
+            while (j<n && pattern[j]=='D'){
+                j++;
+            }
+            int count = j-i;
+            for (int k=max+count; k>max; k--){
+                arr.push_back(k);
+            }
+            max = max+count;
+            i = j-1;
+        }
+
+        string ans;
+        for (int i=0; i<n; i++){
+            ans.push_back('0'+arr[i]);
+        }
+        return ans;
+    }
+};
+
+// ((I))(I)(I)(ID)(IDDD)
+// Reference from 【每日一题】LeetCode 2375. Construct Smallest Number From DI String (aka LC 484. Find Permutation), https://youtu.be/7QXAWuEfWnI.
