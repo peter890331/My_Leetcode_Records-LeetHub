a3327cde9f4148bced8ce51942edbbd03f2f7373
Peter Yu <peter890331@gmail.com>
2025-04-24 22:18:07 +0800

    Time: 19 ms (67.47%), Space: 41.8 MB (71.22%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.cpp
@@ -6,16 +6,19 @@ public:
         for (int i=0; i<n; i++){
             total_count.insert(nums[i]);
         }
-        int total = total_count.size();
-        int ans = 0;
-        for (int left=0; left<n; left++){
-            unordered_set<int> count;
-            for (int right=left; right<n; right++){
-                count.insert(nums[right]);
-                if (count.size() == total){
-                    ans += n - right;
-                    break;
+        int total_elements = total_count.size();
+        unordered_map<int, int> window_count;
+        int left = 0; int window_elements = 0; int ans = 0;
+        for (int right=0; right<n; right++){
+            if (++window_count[nums[right]] == 1){
+                window_elements++;
+            }
+            while (window_elements == total_elements){
+                ans += n - right;
+                if (--window_count[nums[left]] == 0){
+                    window_elements--;
                 }
+                left++;
             }
         }
         return ans;
