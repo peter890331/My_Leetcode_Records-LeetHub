1b0fc914e65fd13f505f87e1063022b48a572fb1
Peter Yu <peter890331@gmail.com>
2025-05-05 17:26:51 +0800

    Time: 1 ms (30.14%), Space: 9.2 MB (26.67%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numTilings(int n) {
+        const int mod = 1e9 + 7;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        vector<long long> dp(n+1);
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        long long sum = dp[0];
+        for (int i=3; i<=n; i++){
+            dp[i] = (dp[i-1] + dp[i-2] + 2 * sum) % mod;
+            sum += dp[i-2];
+        }
+        return dp[n] % mod;
+    }
+};
+
+// dp[i] = dp[i-1] + dp[i-2] + 2 * (dp[0] + dp[1] + ... + dp[i-3])
