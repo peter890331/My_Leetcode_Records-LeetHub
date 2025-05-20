d51a8a2d299753e696f6a960adc727fbbd04845e
Peter Yu <peter890331@gmail.com>
2025-05-20 14:36:20 +0800

    Time: 156 ms (8.72%), Space: 325.4 MB (29.92%) - LeetHub

--- 3355-zero-array-transformation-i/3355-zero-array-transformation-i.cpp
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        vector<int> diff(n, 0);
+        vector<int> diff(n+1, 0);
         for(auto querie: queries){
-            for (int i=querie[0]; i<=querie[1]; i++){
-                diff[i]--;
-            }
+            diff[querie[0]]--;
+            diff[querie[1]+1]++;
         }
+        int temp = 0;
         for (int i=0; i<n; i++){
-            if (nums[i] + diff[i] > 0){
+            temp += diff[i];
+            if (nums[i] + temp > 0){
                 return false;
             }
         }
