5f965e11c2c8495e50ed41a24a2d30c9cf86f2fc
Peter Yu <peter890331@gmail.com>
2025-05-21 15:50:49 +0800

    Time: 0 ms (100%), Space: 18.9 MB (6.61%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,19 +3,19 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<int> temp_n;
-        vector<int> temp_m;
+        unordered_set<int> temp_n;
+        unordered_set<int> temp_m;
         for (int i=0; i<n; i++){
             for (int j=0; j<m; j++){
                 if (matrix[i][j] == 0){
-                    temp_n.push_back(i);
-                    temp_m.push_back(j);
+                    temp_n.insert(i);
+                    temp_m.insert(j);
                 }
             }
         }
         for (int i=0; i<n; i++){
             for (int j=0; j<m; j++){
-                if (find(temp_n.begin(), temp_n.end(), i) != temp_n.end() || find(temp_m.begin(), temp_m.end(), j) != temp_m.end()){
+                if (temp_n.find(i) != temp_n.end() || temp_m.find(j) != temp_m.end()){
                     matrix[i][j] = 0;
                 }
             }
