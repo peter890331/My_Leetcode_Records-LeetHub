342e8ca78518ca976897b961e8f619ece86a9311
Peter Yu <peter890331@gmail.com>
2025-05-26 01:39:09 +0800

    Time: 145 ms (18.72%), Space: 172 MB (26.13%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string, int> count;
+        for (string word: words){
+            count[word]++;
+        }
+        int ans = 0;
+        int same = false;
+        for (string word: words){
+            string reverse_word = word;
+            reverse(reverse_word.begin(), reverse_word.end());
+            if (word != reverse_word){
+                if (count[word] > 0 && count[reverse_word] > 0){
+                    ans += 4;
+                    count[word]--;
+                    count[reverse_word]--;
+                }
+            }
+            else {
+                if (count[word] == 1){
+                    same = true;
+                }
+                else {
+                    int pair = count[word] / 2;
+                    ans += pair * 4;
+                    count[word] -= pair * 2;
+                }
+            }
+        }
+
+        if (same == true){
+            ans += 2;
+        }
+
+        return ans;
+
+    }
+};
