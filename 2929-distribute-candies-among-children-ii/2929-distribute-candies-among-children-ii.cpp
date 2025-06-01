a25a6cc47f225baac48838bc8fad7b716437b500
Peter Yu <peter890331@gmail.com>
2025-06-02 01:56:13 +0800

    Time: 0 ms (100%), Space: 9 MB (57.06%) - LeetHub

--- 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
+++ 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    long long calc(int x) {  // comb(x, 2)
+    long long calc(int x) {                         // comb(x, 2)
         if (x < 0) return 0;
         return 1LL * x * (x - 1) / 2;   // 1LL: long long 的常數 1
     }
     long long distributeCandies(int n, int limit) {
         if (n > 3 * limit) return 0;
-        long long ans = calc(n+2);                // comb(n+2, 2)
+        long long ans = calc(n+2);                  // comb(n+2, 2)
         ans -= 3 * calc(n - limit + 1);
         ans += 3 * calc(n - 2 * limit);
         // ans -= calc(n - 3 * limit - 1);
