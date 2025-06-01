ebaf6d1c78292eb24245b60ccd3e4ea16d3f6dba
Peter Yu <peter890331@gmail.com>
2025-06-02 01:55:13 +0800

    Time: 0 ms (100%), Space: 9 MB (57.06%) - LeetHub

---
+++ 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long calc(int x) {  // comb(x, 2)
+        if (x < 0) return 0;
+        return 1LL * x * (x - 1) / 2;
+    }
+    long long distributeCandies(int n, int limit) {
+        if (n > 3 * limit) return 0;
+        long long ans = calc(n+2);                // comb(n+2, 2)
+        ans -= 3 * calc(n - limit + 1);
+        ans += 3 * calc(n - 2 * limit);
+        // ans -= calc(n - 3 * limit - 1);
+        return ans; 
+    }
+};
+
+// Reference from LeetCode \U0001f7e1 2929. Distribute Candies Among Children II, https://gdst.dev/posts/LC-2929/.
