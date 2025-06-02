70dec306fc3e74b7e531dfea63677efefac002f9
Peter Yu <peter890331@gmail.com>
2025-06-03 00:15:42 +0800

    Time: 0 ms (100%), Space: 22.8 MB (70.62%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -4,13 +4,13 @@ public:
         int n = ratings.size();
         vector<int> candies(n, 1);
 
-        for (int i=1; i<n; i++){
+        for (int i=1; i<n; i++){                // 右邊比左邊高。
             if (ratings[i] > ratings[i-1]){
                 candies[i] = candies[i-1] + 1;
             }
         }
 
-        for (int j=n-2; j>=0; j--){
+        for (int j=n-2; j>=0; j--){             // 左邊比右邊高。
             if (ratings[j] > ratings[j+1]){
                 candies[j] = max(candies[j+1] + 1, candies[j]);
             }
