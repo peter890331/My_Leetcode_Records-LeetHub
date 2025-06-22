2d22315bca51bc5b8066e98bc24f395f3d8ca38d
Peter Yu <peter890331@gmail.com>
2025-06-22 21:22:12 +0800

    Time: 0 ms (100%), Space: 9.8 MB (70.3%) - LeetHub

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.cpp
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.cpp
@@ -7,10 +7,7 @@ public:
         vector<string> ans;
         for (int i=0; i<s.size(); i+=k) {
             string temp = "";
-            for (int j=0; j<k; j++) {
-                temp += s[i+j];
-            }
-            ans.push_back(temp);
+            ans.push_back(s.substr(i, k));
         }
         return ans;
     }
